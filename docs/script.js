6a1ba0c6c740444c05bcffe83f3a4ba37098b1a1
zootedb0t <05kraken@proton.me>
2023-08-25 18:12:26 +0530

    fix: bookmark transparency improve search form

--- docs/script.js
+++ docs/script.js
@@ -1,19 +1,22 @@
 // TODO Fix: Not working. Gettings CORS error
-const imageUrl = "";
-async function fetchWallpaper() {
-  const getRandomImage = await fetch("https://wallhaven.cc/api/v1/search", {
-    method: "GET",
-  });
-  const response = await getRandomImage.json();
-  imageUrl = response.data[0].thumbs.large;
-  var body = document.getElementsByTagName("body")[0];
-  body.style.backgroundImage = "url(imageUrl)";
+// async function fetchWallpaper() {
+function fetchWallpaper() {
+  // const getRandomImage = await fetch("https://wallhaven.cc/api/v1/search", {
+  //   method: "GET",
+  // });
+  // const response = await getRandomImage.json();
+  // const imageUrl = response.data[0].thumbs.large;
+  var body = document.getElementsByTagName('body')[0];
+  body.style.backgroundImage = "url(https://th.wallhaven.cc/lg/jx/jxy6vp.jpg)";
 }
 
 // Seach function
 function doSearch() {
-  const searchTerm = document.getElementById("searchfield").value;
-  const searchParam = `https://www.duckduckgo.com/?q=${searchTerm}`;
-  // const searchParam = `https://www.google.com/search?q=${searchTerm}`;
-  window.open(searchParam);
+  const searchField = document.getElementById("searchfield");
+  const searchTerm = searchField.value.trim();
+  if (searchTerm) {
+    // const searchParam = `https://www.google.com/search?q=${searchTerm}`;
+    window.open(`https://www.duckduckgo.com/?q=${searchTerm}`);
+  }
+  searchField.value = "";
 }
