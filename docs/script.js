647d431e44179b7077bb1ae33952342af8ca6f75
zootedb0t <05kraken@proton.me>
2023-08-29 14:49:42 +0530

    code cleanup

--- docs/script.js
+++ docs/script.js
@@ -1,11 +1,5 @@
-// TODO Fix: Not working. Gettings CORS error
-// async function fetchWallpaper() {
+// Show random wallpaper from web
 function fetchWallpaper() {
-  // const getRandomImage = await fetch("https://wallhaven.cc/api/v1/search", {
-  //   method: "GET",
-  // });
-  // const response = await getRandomImage.json();
-  // const imageUrl = response.data[0].thumbs.large;
   var body = document.getElementsByTagName("body")[0];
   body.style.backgroundImage = "url(https://picsum.photos/1920/1080.webp)";
 }
