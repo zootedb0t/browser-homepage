7856b36418a82955878677ddce3e0e6ee01854b6
zootedb0t <05kraken@proton.me>
2023-10-05 15:58:30 +0530

    add python function docstring

--- imageofday.py
+++ imageofday.py
@@ -6,6 +6,9 @@ server_response = requests.get(APIURL)
 
 
 def get_file_url():
+    """
+    Return image url
+    """
     if server_response.status_code == 200:
         return server_response.json()["data"][0].get("path")
     return "https://w.wallhaven.cc/full/3k/wallhaven-3k6ply.png"
