d1669c34f2fb0f923d5caa6f146f6f2141707aa7
zootedb0t <05kraken@proton.me>
2023-10-25 06:18:29 +0530

    update python script

--- imageofday.py
+++ imageofday.py
@@ -5,18 +5,18 @@ import requests
 
 APIURL = "https://wallhaven.cc/api/v1/search?q=anime&sorting=random&atleast=1920x1080"
 
-server_response = requests.get(APIURL)
-
 
 def get_file_url():
     """
     Return image url
     """
+    server_response = requests.get(APIURL)
     if server_response.status_code == 200:
         return server_response.json()["data"][0].get("path")
-    return "https://w.wallhaven.cc/full/3k/wallhaven-3k6ply.png"
+    return "https://w.wallhaven.cc/full/3k/wallhaven-3k6ply.png"  # Fallback URL
 
 
+# Save file
 file_url = get_file_url()
 r = requests.get(file_url)
 
